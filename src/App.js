f47e6498b8cacf3893cc924247cd52a428129a76
ahjsuy <92120716+ahjsuy@users.noreply.github.com>
2021-11-16 11:48:22 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import Game from './Game';
+import PlaySound from './PlaySound';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state={
+      gameStarted: false,
+      numPlayers: 6,
+    }
+    this.startGame = this.startGame.bind(this)
+    this.setNumPlayers = this.setNumPlayers.bind(this)
+  }
+  startGame() {
+    this.setState({gameStarted: true})
+  }
+  setNumPlayers(event) {
+    this.setState({numPlayers: parseInt(event.target.value,10)})
+  }
+
+  render()  {
+    return (
+      <div>
+        <PlaySound />
+        <h1>POKER</h1>
+        {this.state.gameStarted ? <Game numPlayers={this.state.numPlayers}/> :
+          <div>
+            <p>Enter number of players(minimum 4, maximum 20)</p>
+            <input type="text" value={this.state.numPlayers} onChange={this.setNumPlayers} />
+            <button onClick={this.startGame}>START</button>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
